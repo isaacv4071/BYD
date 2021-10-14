a87cba5cc604c4bb5e8e6eebde5003dafacb0bba
isaacv4071 <isaacv4071@gmail.com>
2021-10-14 14:44:15 -0500

    Modulo productos listo

--- routes/producto.py
+++ routes/producto.py
@@ -4,6 +4,18 @@ from flask import render_template
 productos = flask.Blueprint('productos', __name__)
 
 #ruta para renderizar la pagina principal de producto
-@productos.route("/producto",methods=['GET'])
+@productos.route("/products",methods=['GET'])
 def product():
     return render_template('product/producto.html')
+
+@productos.route("/addproducto",methods=['GET'])
+def addproduct():
+    return render_template('product/addproduct.html')
+
+@productos.route("/editproducto",methods=['GET'])
+def editproduct():
+    return render_template('product/editproduct.html')
+
+@productos.route("/product",methods=['GET'])
+def viewproduct():
+    return render_template('product/productpage.html')
