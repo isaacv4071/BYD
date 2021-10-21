262b066ef7d9578bac42c63acd6f57b72d6b889e
isaacv4071 <isaacv4071@gmail.com>
2021-10-21 01:37:18 -0500

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 
 # ruta para renderizar el login
-@app.route("/",methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
